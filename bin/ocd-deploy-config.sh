b33f2a11e886ccedb68da2cb958e10680586f2ae
Simon Massey <simon@simonmassey.org>
2019-02-15 20:22:28 +0000

    making it easier to debug

--- bin/ocd-deploy-config.sh
+++ bin/ocd-deploy-config.sh
@@ -65,12 +65,16 @@ if ! git commit -am "$MESSAGE" 1>/dev/null; then
   exit 12
 fi
 
-if ! git push origin "$TAG" 1>/dev/null; then
+BRANCH=$(date +"%Y%m%d_%H%M%S")
+
+if ! git push origin "$BRANCH" 1>/dev/null; then
   >&2 echo "ERROR failed to git push origin $TAG"
   exit 13
 fi
 
 if ! hub pull-request -m "$MESSAGE"; then
-  >&2 echo "ERROR failed to hub pull-request -m $MESSAGE"
+  >&2 echo "ERROR failed to hub pull-request -m $MESSAGE on branch $BRANCH"
   exit 14
+else
+  git branch -d "$BRANCH"
 fi
