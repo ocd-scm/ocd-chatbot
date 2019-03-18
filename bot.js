5f1ef9bb718cf2a7425e889da0bb49170ae29ec5
Simon Massey <simon@simonmassey.org>
2019-03-18 21:59:51 +0000

    removed logging of secrets

--- bot.js
+++ bot.js
@@ -69,9 +69,8 @@ var bot_options = {
     studio_command_uri: process.env.studio_command_uri
 };
 
-console.log("bot_options: ");
-
-console.dir(bot_options, {depth: null, colors: true})
+// console.log("bot_options: ");
+// console.dir(bot_options, {depth: null, colors: true})
 
 // Use a mongo database if specified, otherwise store in a JSON file local to the app.
 // Mongo is automatically configured when deploying to Heroku
